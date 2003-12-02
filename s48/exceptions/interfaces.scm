eeb9ccd78a030fa1496ad145f1dab30abc51682a
Eric Knauel <knauel@informatik.uni-tuebingen.de>
2003-12-02 09:48:46 +0000

    added SRFI-34/35: conditions and exception handling

---
+++ s48/exceptions/interfaces.scm
@@ -0,0 +1,24 @@
+(define-interface srfi-34-interface
+  (export
+   raise
+   with-exception-handler
+   with-exception-handlers
+   (guard :syntax)))
+
+(define-interface srfi-35-interface
+  (export
+   make-condition-type
+   condition-type?
+   make-condition
+   condition?
+   condition-has-type?
+   condition-ref
+   make-compound-condition
+   extract-condition
+   (define-condition-type :syntax)
+   (condition :syntax)
+   &condition
+   &message message-condition? condition-message
+   &serious serious-condition?
+   &error error?))
+
---
+++ s48/exceptions/packages.scm
@@ -0,0 +1,11 @@
+(define-structure srfi-34 srfi-34-interface
+  (open scheme
+	signals)
+  (files srfi-34))
+
+(define-structure srfi-35 srfi-35-interface
+  (open scheme
+	signals
+	srfi-1
+	srfi-9)
+  (files srfi-35))
