d97fa40456c31b0aec3b908307afa4bf052c033f
Anthony Carrico <acarrico@memebeam.org>
2004-03-10 17:25:20 +0000

    Install the NEWS file.

--- s48/exceptions/pkg-def.scm
+++ s48/exceptions/pkg-def.scm
@@ -2,6 +2,7 @@
   (0 1)
   ()
   (install-file "load.scm" 'base)
+  (install-file "NEWS" 'doc)
   (install-string (COPYING) "COPYING" 'doc)
   (install-file "restart.scm" 'scheme)
   (install-file "srfi-34.scm" 'scheme)
