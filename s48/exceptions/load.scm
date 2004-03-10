22d470fc0881ae5358c801e4f1e180f91cf04bb4
Anthony Carrico <acarrico@memebeam.org>
2004-03-10 16:12:09 +0000

    Changed to new package system.

--- s48/exceptions/load.scm
+++ s48/exceptions/load.scm
@@ -26,3 +26,20 @@
   (export
    (raise-restartable :syntax)
    (restart :syntax)))
+
+(define-structure srfi-34 srfi-34-interface
+  (open scheme
+	signals)
+  (files srfi-34))
+
+(define-structure srfi-35 srfi-35-interface
+  (open scheme
+	signals
+	srfi-1
+	srfi-9)
+  (files srfi-35))
+
+(define-structure srfi-34-restart srfi-34-restart-interface
+  (open scheme
+	srfi-34 srfi-35)
+  (files restart))
---
+++ s48/exceptions/pkg-def.scm
@@ -0,0 +1,8 @@
+(define-package "exceptions"
+  (0 1)
+  ()
+  (install-file "load.scm" 'base)
+  (install-string (COPYING) "COPYING" 'doc)
+  (install-file "restart.scm" 'scheme)
+  (install-file "srfi-34.scm" 'scheme)
+  (install-file "srfi-35.scm" 'scheme))
