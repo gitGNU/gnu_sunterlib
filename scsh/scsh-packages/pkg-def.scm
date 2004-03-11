c30c8345ac4715218fab975c8bd8c4c4d8449c48
Anthony Carrico <acarrico@memebeam.org>
2004-03-11 19:01:40 +0000

    Added files.

---
+++ scsh/scsh-packages/pkg-def.scm
@@ -0,0 +1,13 @@
+(define-package "scsh-packages"
+  (0 0)
+  ()
+  (install-file "load.scm" 'base)
+  (install-file "README" 'doc)
+  (install-file "NEWS" 'doc)
+  (install-string (COPYING) "COPYING" 'doc)
+  (install-file "scheme/install-lib/configure.scm" 'scheme)
+  (install-file "scheme/install-lib/install-lib-module.scm"
+                'scheme)
+  (install-file "scheme/install-lib/install-lib.scm" 'scheme)
+  (install-file "scheme/install-lib/install-pkg" 'scheme)
+  (install-file "doc/latex/proposal.tex" 'misc-shared))
